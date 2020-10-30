b377bb6ec1247fa5598561051d0f612250c47e97
rohitkandiyal <kandiyalrohit@gmail.com>
2020-10-31 04:59:52 +0530

    Exception handling enabled

--- self_syntax_codes/website_monitoring/url_monitor.py
+++ self_syntax_codes/website_monitoring/url_monitor.py
@@ -1,22 +1,24 @@
-from http.client import HTTPConnection
+from http.client import HTTPConnection, HTTPException, socket
 import logging, sys
 
 
 url_list=["python.org","http://python.org","www.python.org"]
-logging.basicConfig(filename='abc.log', level=logging.INFO, format='%(asctime)s %(levelname)s: %(message)s', 
+logging.basicConfig(filename='site_access.log', level=logging.INFO, format='%(asctime)s %(levelname)s: %(message)s', 
             datefmt='%Y-%m-%d %H:%M:%S')
 
 def fetch_response_status(url):
-    try
+    try:
         connection = HTTPConnection(url)
         connection.request("GET", "/")
         response = connection.getresponse()
         return response.status
-    except HTTPException as error:
-        logging.error('HTTP connection threw error:', error)
-    except Exception as e:
+    except HTTPException:
+        logging.error('HTTPException error')
+    except socket.error:
+        logging.error('Socket error')
+    except:
         logging.info('Undefined URL:', url)
-        logging.error('Error is:', e)
+        logging.error('HTTP connection Error')
 
 def website_check(url):
     resp = fetch_response_status(url)
@@ -24,12 +26,12 @@ def website_check(url):
         return "Success"
     else:
         return "Failure"
-        logging.error("Website unreachable..")
+        logging.error("Website unreachable:", url)
 
 def internet_available():
     try:
         r=website_check("www.google.com")
-    except Exception:
+    except:
         logging.error("Website response failed..") 
     
     if (r == "Success"):
@@ -37,7 +39,14 @@ def internet_available():
         return True
     else:
         logging.error("Internet Unavailable")
+        return False
 
 
-for url in url_list:
+logging.info("+++++++++++++++++++++++++++++++++Starting URL health check+++++++++++++++++++++++++++++++++++++++++")
+if internet_available():
+    print("PASS")
+else:
+    print("FAIL")
+#for url in url_list:
+    
 
