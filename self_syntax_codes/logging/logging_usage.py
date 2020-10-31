2eeb67c1681f453411290fa3c7334ba44b20db4d
rohitkandiyal <kandiyalrohit@gmail.com>
2020-11-01 02:33:15 +0530

    Url monitoring:email disabled

--- self_syntax_codes/logging/logging_usage.py
+++ self_syntax_codes/logging/logging_usage.py
@@ -2,13 +2,9 @@ import logging
 
 logging.basicConfig(filename='abc.log', level=logging.INFO, format='%(asctime)s %(levelname)s: %(message)s', 
             datefmt='%Y-%m-%d %H:%M:%S')
-
+a="defined"
 logging.debug("This is debug")
 logging.info('This is info')
 logging.warning('This is warning')
-logging.error('This is error')
+logging.error('This is error {}'.format(a))
 
-def abc():
-    logging.info(abc.__name__, "This is function info")
-
-abc
--- self_syntax_codes/website_monitoring/url_monitor.py
+++ self_syntax_codes/website_monitoring/url_monitor.py
@@ -1,8 +1,12 @@
+#!/usr/bin/python
+
 from http.client import HTTPConnection, HTTPException, socket
+from smtplib import SMTP
+import requests
 import logging, sys
 
-
-url_list=["python.org","http://python.org","www.python.org"]
+#https://na1.dev.nice-incontact.com/    https://na12.dev.nice-incontact.com/
+url_list=["python.org","http://python.org","www.python.org","https://na1.dev.nice-incontact.com/","na1.dev.nice-incontact.com/","www.na12.dev.nice-incontact.com/"]
 logging.basicConfig(filename='site_access.log', level=logging.INFO, format='%(asctime)s %(levelname)s: %(message)s', 
             datefmt='%Y-%m-%d %H:%M:%S')
 
@@ -17,36 +21,85 @@ def fetch_response_status(url):
     except socket.error:
         logging.error('Socket error')
     except:
-        logging.info('Undefined URL:', url)
+        logging.error('Invalid URL:', url)
         logging.error('HTTP connection Error')
 
+def check_http_url(url_to_validate):
+    try:
+        response = requests.get(url_to_validate)
+        logging.info("URL: {} is valid and exists on the internet".format(url_to_validate))
+        return response.status_code
+    except requests.ConnectionError as exception:
+        logging.error("URL: {} does not exist on Internet".format(url_to_validate))
+
+def set_http_url(url):
+    if (url.startswith("http://") or url.startswith("https://")):
+        return url
+    elif (url.startswith("www.")):
+        return url.replace("www.","http://",1)
+    else:
+        return "{}{}".format("http://",url)
+
 def website_check(url):
-    resp = fetch_response_status(url)
+    url=set_http_url(url)
+    resp = check_http_url(url)
+    '''
+    if (url.startswith("http")):
+        resp = check_http_url(url)
+    else:
+        resp = fetch_response_status(url)
+    '''
     if (resp == 200):
         return "Success"
     else:
         return "Failure"
-        logging.error("Website unreachable:", url)
+        logging.error("URL unreachable:", url)
 
 def internet_available():
     try:
-        r=website_check("www.google.com")
+        r=website_check("http://google.com")
+        r1=website_check("http://facebook.com")
     except:
         logging.error("Website response failed..") 
     
-    if (r == "Success"):
+    if (r == "Success") or (r1 == "Success"):
         logging.info("Internet Connection Established")
         return True
     else:
-        logging.error("Internet Unavailable")
         return False
 
+def send_email(receiver,subject):
+    port = 587
+    smtp_server = "smtp.gmail.com"
+    e_sender = "kandiyalrohitabcd@gmail.com"
+    e_receiver = receiver
+    password = "Symantec@123"
+    email_content = "Subject: {}".format(subject)
+    with SMTP(smtp_server, port) as server:
+        try:
+            server.starttls()
+            server.login("kandiyalrohitabcd", password)
+            server.sendmail(e_sender, e_receiver, email_content)
+        except SMTPException:
+            logging.error('SMTPException error')
+        except:
+            logging.error('SMTP connection Error')
 
-logging.info("+++++++++++++++++++++++++++++++++Starting URL health check+++++++++++++++++++++++++++++++++++++++++")
+
+logging.info("++++++++++++++++++++++Starting URL {} health check++++++++++++++++++++++".format(url_list))
 if internet_available():
-    print("PASS")
+    for url in url_list:
+        url = set_http_url(url)
+        logging.info("Checking URL {} ".format(url))
+        if (website_check(url) == "Success"):
+            logging.info("{0} is ACTIVE.".format(url))
+        else:
+            logging.error("{0} is INACTIVE.".format(url))
+            '''subject_content="{} : URL IS UNRESPONSIVE".format(url)
+            receiver_list=["kandiyalrohit@gmail.com"]
+            for receiver in receiver_list:
+                send_email(receiver,body)'''
 else:
-    print("FAIL")
-#for url in url_list:
-    
+    logging.error("Internet Unavailable")
 
+    
